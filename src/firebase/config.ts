48de861d35f19c5d391ed582df29faf56241dccb
Colton Thomas <colton615@gmail.com>
2025-11-08 03:23:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6421410903-7db88",
+  "appId": "1:1011529103883:web:1070d1996dcd9d8b71a50e",
+  "apiKey": "AIzaSyAvyII9RPT5S1dClN4mbZijk_bPsB0ClTk",
+  "authDomain": "studio-6421410903-7db88.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1011529103883"
+};
